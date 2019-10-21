fa62ba0ba9b4a9b996f66b75dcf09bd3773b8701
johanseo <trullo393@gmail.com>
2019-10-22 02:39:45 +0900

    2.2 JUnit4를 활용해 main() 메소드 문제점 극복

---
+++ src/Step2/tests/CalculatorTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+/* JUnit4 설정법
+ * junit4 라이브러리르 import한다.
+ * 1. 테스트 파일을 담을 디렉터리를 생성한다.
+ * 2. 테스트할 클래스파일이 담긴 상위 폴더 우클릭 후 Open Module Settings(F4) 후 1번에서 생성한 디렉터리를 Test로 mark 한다.
+ * 3. mark한 디렉터리에 자바클래스를 생성후 함수에 @Test 어노테이션을 추가한후 출력을 확인한다.
+ */
+public class CalculatorTest {
+    @Test
+    public void add() {
+        Calculator cal = new Calculator();
+        System.out.println(cal.add(8,3));
+    }
+
+    @Test
+    public  void subtract(){
+        Calculator cal = new Calculator();
+        System.out.println(cal.subtract(7,3));
+    }
+}
---
+++ src/Step2/tests/CalculatorTest2.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/* 2.2.2 결과값을 눈이 아닌 프로그램 통해 자동화
+ * assertEquals는 Static 메소드라 import static으로 메소드를 import한 후 위와 같이 구현할 수 있다.
+ * 메소드의 첫 번째 인자는 기대하는 결과 값이고 두 번째 인자는 프로덕션 코드의 메소드를 실행한 결과 값이다.
+ * 이외에도 ture/false를 확인하는 assertTrue(), assertFasle() 결과 값이 null 유무를 판단하는 assertNull(), assertNotNull()
+ * 배열값이 같은지를 검증하는 assertArrayEquals() 메소드 등을 제공한다.
+ */
+public class CalculatorTest2 {
+    @Test
+    public void add() {
+        Calculator cal = new Calculator();
+        assertEquals(7, cal.add(4,3));
+    }
+
+    @Test
+    public void subtract(){
+        Calculator cal = new Calculator();
+        assertEquals(5,cal.subtract(6,3));
+    }
+}
